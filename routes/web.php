0d2468b436b99c1b70e3b96786c7e2d9c96e116a
unknown <cruchyn@gmail.com>
2026-03-04 22:08:28 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/patients');
 });
 
 Route::resource('patients', PatientController::class);
